9d2c7808972cb54a48fa689c5c4d772da668c320
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-03-05 19:17:33 -0600

    finished with no styling yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
+import NasaData from "./Components/NasaData"
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+<h1>Nasa Photo of the Day!</h1>
+    <NasaData/>
     </div>
   );
 }
-
 export default App;
